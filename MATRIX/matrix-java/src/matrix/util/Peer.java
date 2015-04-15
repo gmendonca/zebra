5bbaccaee9101db55316bb4754441ff741415908
Gustavo Mendonca <guzzgm@gmail.com>
2015-04-14 23:45:23 -0500

    More contribuitions to MATRIX in Java

--- MATRIX/matrix-java/src/matrix/util/Peer.java
+++ MATRIX/matrix-java/src/matrix/util/Peer.java
@@ -16,14 +16,5 @@ public interface Peer {
 	public void submitTaskWc(List<TaskMsg> taskMsg, int randomScheduler);
 	
 	public void doMonitoring();
-	
-	public List<String> taskList = null;
-	
-	public List<TaskMsg> tasks = null;
-	
-	public long startTime = 0, stopTime = 0;
-	
-	public OutputStream clientLogOS = null;
-	public OutputStream systemLogOS = null;
 
 }
